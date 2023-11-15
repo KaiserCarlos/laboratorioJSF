ea1b30d5084b6836f20e15f02dd5ee80090bed84
KaiserCarlos <carlos.vilarchao710@gmail.com>
2023-11-15 11:45:23 +0100

    nooooooooooooooooooooooooooooooooOOOO funcionaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa oooooooooooooooooooooooooooooooooooooaaaaaaaaaaaaaaaaaaaaaaaaaoooooooooooooooooooooooooooooooooooooooooooooooooaaaaaaaaaaaaaaaaaaa putas dependencias

--- src/main/java/com/example/laboratoriojsf/HelloServlet.java
+++ src/main/java/com/example/laboratoriojsf/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Polla!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
